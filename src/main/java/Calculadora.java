75f4ee54dfcdfa7d2a949f3167e54cea2211f37b
Miguel Ángel <Miguel ngel@DESKTOP-M0PO337>
2021-03-08 17:09:04 +0000

    paso 1 y 2

---
+++ src/main/java/Calculadora.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Miguel Ángel
+ */
+public class Calculadora {
+    private int operador1;
+    private int operador2;
+    private int operacion;
+}
