d9584e0b39647f8a7188252a9099492c04613691
Miguel Ángel <Miguel ngel@DESKTOP-M0PO337>
2021-03-08 17:20:31 +0000

    paso 3 y 4

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -10,6 +10,30 @@
  */
 public class Calculadora {
     private String operador1;
+
+    public String getOperador1() {
+        return operador1;
+    }
+
+    public void setOperador1(String operador1) {
+        this.operador1 = operador1;
+    }
+
+    public String getOperador2() {
+        return operador2;
+    }
+
+    public void setOperador2(String operador2) {
+        this.operador2 = operador2;
+    }
+
+    public String getOperacion() {
+        return operacion;
+    }
+
+    public void setOperacion(String operacion) {
+        this.operacion = operacion;
+    }
     private String operador2;
     private String operacion;
 }
